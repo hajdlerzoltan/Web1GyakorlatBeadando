f48c3c89ce30d838bf6e62ec8211257f8a1b36dc
hajdlerzoltan <hajdlerzoltan@gmail.com>
2026-06-20 23:19:25 +0200

    adding db and config files

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASE_URL', '/Web1Gyakorlat/');
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'Web1Gyakorlat';
+const DB_USER = 'web1eloadas';
+const DB_PASS = 'NJE_WEB_1';
+const APP_NAME = 'Budapest Kávézó';
+const OWNER_ADDRESS = '1051 Budapest, Vörösmarty tér 1.';
